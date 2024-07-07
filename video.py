d353f8089e5cca835774301d010d810d89a0479d
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-07 22:52:13 +0530

    Update video.py

--- video.py
+++ video.py
@@ -17,7 +17,7 @@ aria2 = aria2p.API(
     )
 )
 async def download_video(url, reply_msg, user_mention, user_id):
-    response = requests.get(f"https://teraboxvideodownloader.nepcoderdevs.workers.dev/?url={url}")
+    response = requests.get(f"https://tera.instavideosave.com/?url={url}")
     response.raise_for_status()
     data = response.json()
 
