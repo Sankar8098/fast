47a7494cc827cbbd9b5a47b7b33dab45db20d05f
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-07 21:04:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python terabox.py
+#!/bin/sh
+
+# Start aria2c with the appropriate options
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon
+
+# Run your Python script
+python terabox.py
