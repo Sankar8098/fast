4f5a485e99111df1794120b92a27370e3e0cbd6c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-03-22 22:48:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Ensure moviepy installs correctly
+# Install missing dependencies
 pip install --no-cache-dir --force-reinstall -r requirements.txt
+
+# Ensure aria2 is installed
+command -v aria2c >/dev/null 2>&1 || { 
+    echo "Installing aria2...";
+    apt update && apt install -y aria2 || apk add --no-cache aria2;
+}
 
 # Start aria2c in the background
 aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon &
